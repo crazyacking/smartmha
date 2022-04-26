d25a9d9ab43830b23f8a17eb64a5a655aaa86be5
Mengyu <shanbiao.jsb@alibaba-inc.com>
2022-04-26 20:05:59 +0800

    feat: add options for ClusteringFlags group

--- src/main.cc
+++ src/main.cc
@@ -60,6 +60,66 @@ int main(int argc, char *argv[]) {
             "post-leader-start-command",
             "The shell command when post leader started.",
             cxxopts::value<std::string>()->default_value("default=http://localhost:2380")
+    )(
+            "pre-leader-start-command",
+            "The shell command when pre leader start.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "pre-leader-stop-command",
+            "The shell command when pre leader stop.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "post-leader-stop-command",
+            "The shell command when post leader stopped.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "pre-follower-start-command",
+            "The shell command when pre follower start.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "post-follower-start-command",
+            "The shell command when post follower started.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "pre-follower-stop-command",
+            "The shell command when pre follower stop.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "post-follower-stop-command",
+            "The shell command when post follower stopped.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "pre-candidate-start-command",
+            "The shell command when pre candidate start.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "post-candidate-start-command",
+            "The shell command when post candidate started.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "pre-candidate-stop-command",
+            "The shell command when pre candidate stop.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "post-candidate-stop-command",
+            "The shell command when post candidate stopped.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "pre-leader-change-command",
+            "The shell command when pre leader change.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "post-leader-change-command",
+            "The shell command when post leader changed.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "initial-cluster-state",
+            "Initial cluster state ('new' or 'existing'). Set to new for all members present during initial static. If this option is set to existing, polar-ha will attempt to join the existing cluster. If the wrong value is set, polar-ha will attempt to start but fail safely.",
+            cxxopts::value<std::string>()->default_value("")
+    )(
+            "initial-cluster-token",
+            "Initial cluster token for the cluster during bootstrap.",
+            cxxopts::value<std::string>()->default_value("smart-mha-cluster")
     );
 
     auto parseResult = options.parse(argc, argv);
